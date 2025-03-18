c50d141d2946fc60161765cb6610162137059c52
Hayden Bleasel <hello@haydenbleasel.com>
2025-03-18 11:46:21 -0700

    Build fixes

--- next.config.ts
+++ next.config.ts
@@ -4,57 +4,9 @@ import withBundleAnalyzer from '@next/bundle-analyzer';
 import { withSentryConfig } from '@sentry/nextjs';
 import type { NextConfig } from 'next';
 
-export const sentryConfig: Parameters<typeof withSentryConfig>[1] = {
-  org: env.SENTRY_ORG,
-  project: env.SENTRY_PROJECT,
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  /*
-   * For all available options, see:
-   * https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-   */
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  /*
-   * Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-   * This can increase your server load as well as your hosting bill.
-   * Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-   * side errors will fail.
-   */
-  tunnelRoute: '/monitoring',
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  /*
-   * Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-   * See the following for more information:
-   * https://docs.sentry.io/product/crons/
-   * https://vercel.com/docs/cron-jobs
-   */
-  automaticVercelMonitors: true,
-};
-
-export const withSentry = (sourceConfig: object): object => {
-  const configWithTranspile = {
-    ...sourceConfig,
-    transpilePackages: ['@sentry/nextjs'],
-  };
-
-  return withSentryConfig(configWithTranspile, sentryConfig);
-};
-
-export const withLogging = (config: object): object => {
-  return withLogtail(config);
-};
-
 const otelRegex = /@opentelemetry\/instrumentation/;
 
-export const config: NextConfig = {
+let config: NextConfig = withLogtail({
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
@@ -91,14 +43,52 @@ export const config: NextConfig = {
 
   // This is required to support PostHog trailing slash API requests
   skipTrailingSlashRedirect: true,
-};
+});
 
 if (env.VERCEL) {
-  nextConfig = withSentry(nextConfig);
+  const configWithTranspile = {
+    ...config,
+    transpilePackages: ['@sentry/nextjs'],
+  };
+
+  config = withSentryConfig(configWithTranspile, {
+    org: env.SENTRY_ORG,
+    project: env.SENTRY_PROJECT,
+
+    // Only print logs for uploading source maps in CI
+    silent: !process.env.CI,
+
+    /*
+    * For all available options, see:
+    * https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+    */
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    /*
+    * Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    * This can increase your server load as well as your hosting bill.
+    * Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    * side errors will fail.
+    */
+    tunnelRoute: '/monitoring',
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    /*
+    * Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    * See the following for more information:
+    * https://docs.sentry.io/product/crons/
+    * https://vercel.com/docs/cron-jobs
+    */
+    automaticVercelMonitors: true,
+  });
 }
 
 if (env.ANALYZE === 'true') {
-  nextConfig = withBundleAnalyzer()(nextConfig);
+  config = withBundleAnalyzer()(config);
 }
 
-export default nextConfig;
+export default config;
