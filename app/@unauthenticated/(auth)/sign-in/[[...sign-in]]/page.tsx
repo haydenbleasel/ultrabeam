0968feb98f56b92e30229facad8a5356978df317
Hayden Bleasel <hello@haydenbleasel.com>
2025-03-28 10:12:56 -0700

    Revert "Disable auth pages"
    This reverts commit 74aca9e158886a969f3c8e0a6b0501df59fc47c2.

--- app/@unauthenticated/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/@unauthenticated/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,15 +1,12 @@
 import { createMetadata } from '@/lib/seo/metadata';
-// import { SignIn } from '@clerk/nextjs';
+import { SignIn } from '@clerk/nextjs';
 import type { Metadata } from 'next';
-import { notFound } from 'next/navigation';
 
 const title = 'Welcome back';
 const description = 'Enter your details to sign in.';
 
 export const metadata: Metadata = createMetadata({ title, description });
 
-// const SignInPage = () => <SignIn />;
-
-const SignInPage = () => notFound();
+const SignInPage = () => <SignIn />;
 
 export default SignInPage;
