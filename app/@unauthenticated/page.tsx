88b52a4800505a02e87d8fe937ba7d643c0e93f2
Hayden Bleasel <hello@haydenbleasel.com>
2025-03-28 10:12:56 -0700

    Revert disabling auth
    This reverts commit e6b522fc1d86af54b974fc488901bbb256f3de03.
    
    Revert "Disable auth pages"
    
    This reverts commit 74aca9e158886a969f3c8e0a6b0501df59fc47c2.

--- app/@unauthenticated/page.tsx
+++ app/@unauthenticated/page.tsx
@@ -1,10 +1,11 @@
 import { Features } from './components/features';
 import { Games } from './components/games';
+import { Header } from './components/header';
 import { Hero } from './components/hero';
 
 const Home = () => (
-  <div className="grid gap-16 py-16">
-    {/* <Header /> */}
+  <div className="grid gap-16 pb-16">
+    <Header />
     <Hero />
     <Games />
     <Features />
