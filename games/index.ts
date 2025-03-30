846ff55fb1cd499204b926ebd2c0e63183606201
Hayden Bleasel <hello@haydenbleasel.com>
2025-03-29 21:06:15 -0700

    Update index.ts

--- games/index.ts
+++ games/index.ts
@@ -1,8 +1,6 @@
-import { enshrouded } from './enshrouded';
-import { minecraft } from './minecraft';
-import { palworld } from './palworld';
-import { rust } from './rust';
-import { terraria } from './terraria';
-import { valheim } from './valheim';
-
-export const games = [enshrouded, minecraft, palworld, rust, terraria, valheim];
+export { enshrouded } from './enshrouded';
+export { minecraft } from './minecraft';
+export { palworld } from './palworld';
+export { rust } from './rust';
+export { terraria } from './terraria';
+export { valheim } from './valheim';
